404e786e56790bd2b143ed0512dd9602018197e6
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-11-23 21:33:39 +0000

    created a welcome user on navbar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # Allow users to update their profile without providing the current password
   def update_resource(resource, params)
     if params[:password].blank? && params[:password_confirmation].blank?
+      # Update without password if no new password is provided
       resource.update_without_password(params.except(:current_password))
     else
-      resource.update(params)
+      # Update with password validation if the user provided a new password
+      resource.update_with_password(params)
     end
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,33 @@
 Rails.application.routes.draw do
+  # Root route
+  root 'locations#index'
+
+  # Devise Routes (Custom Controller for Registrations)
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  # Locations Routes
   resources :locations do
     resource :favorite, only: [:create, :destroy]
 
-    collection do
-      get :filter # Custom route for filtering by activity and amenities
-    end
+    # Filter is part of the index action, so no need for a separate route
   end
 
+  # Favorites index (list of user's favorite locations)
   resources :favorites, only: [:index]
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }  
-
-  root 'locations#index'
-
-  # Attributes are only needed as a collection to show available activities/amenities
+  # Attributes are used to show available activities/amenities (collection level route)
   resources :attributes, only: [] do
     collection do
       get :available
     end
   end
-  
+
+  # LocationAttributes management
   resources :location_attributes, only: [:create, :destroy]
 
+  # Admin namespace with authentication restriction
   authenticate :user, lambda { |u| u.admin? } do
     namespace :admin do
       resources :locations, only: [:index, :new, :create, :edit, :update, :destroy]
@@ -31,4 +35,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
