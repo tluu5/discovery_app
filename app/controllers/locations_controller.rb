bc795128da161181be19d22f9d65c23c7c068e3f
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-11-27 21:34:14 +0000

    update the location cards using carousel

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,26 +4,26 @@ class LocationsController < ApplicationController
   # GET /locations or /locations.json
   def index
     @locations = Location.all
-  
+
     # Filter by activity
     if params[:activities].present?
       @locations = @locations.joins(location_attributes: :feature)
-                             .where(attributes: { name: params[:activities], category: 'Activity' })
+                             .where(attributes: { name: params[:activities], category: "Activity" })
                              .distinct
     end
-  
+
     # Filter by amenities
     if params[:amenities].present?
       @locations = @locations.joins(location_attributes: :feature)
-                             .where(attributes: { name: params[:amenities], category: 'Amenity' })
+                             .where(attributes: { name: params[:amenities], category: "Amenity" })
                              .distinct
     end
-  
+
     # Search by name
     if params[:search].present?
       @locations = @locations.where("name ILIKE ?", "%#{params[:search]}%")
     end
-  end  
+  end
 
   # GET /locations/1 or /locations/1.json
   def show
@@ -43,7 +43,7 @@ class LocationsController < ApplicationController
   # POST /locations or /locations.json
   def create
     @location = Location.new(location_params)
-  
+
     respond_to do |format|
       if @location.save
         assign_attributes_to_location
@@ -54,13 +54,22 @@ class LocationsController < ApplicationController
         format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
-  end  
+  end
 
   # PATCH/PUT /locations/1 or /locations/1.json
   def update
     respond_to do |format|
       if @location.update(location_params)
         assign_attributes_to_location
+
+        # Remove only selected images
+        if params[:location][:remove_images].present?
+          params[:location][:remove_images].each do |image_id|
+            image = @location.images.find_by(id: image_id)
+            image.purge if image.present?
+          end
+        end
+
         format.html { redirect_to location_url(@location), notice: "Location was successfully updated." }
         format.json { render :show, status: :ok, location: @location }
       else
@@ -68,7 +77,7 @@ class LocationsController < ApplicationController
         format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
-  end  
+  end
 
   # DELETE /locations/1 or /locations/1.json
   def destroy
@@ -98,21 +107,22 @@ class LocationsController < ApplicationController
       # TODO: add back activities / amenities
       # activities: [],
       # amenities: [],
-      images: [])
+      images: []
+    )
   end
 
   def assign_attributes_to_location
     if params[:location][:activities].present?
       # Find or create activities based on the provided names
-      activities = Attribute.where(name: params[:location][:activities], category: 'Activity')
+      activities = Attribute.where(name: params[:location][:activities], category: "Activity")
       @location.activities = activities
     else
       @location.activities = [] # Clear all activities if none are selected
     end
-  
+
     if params[:location][:amenities].present?
       # Find or create amenities based on the provided names
-      amenities = Attribute.where(name: params[:location][:amenities], category: 'Amenity')
+      amenities = Attribute.where(name: params[:location][:amenities], category: "Amenity")
       @location.amenities = amenities
     else
       @location.amenities = [] # Clear all amenities if none are selected
