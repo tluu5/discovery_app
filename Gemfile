feaef8ca14927a1c69367fede1ea666265b3a478
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-12-01 16:53:21 +0000

    adding tests

--- Gemfile
+++ Gemfile
@@ -73,6 +73,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'database_cleaner-active_record'
 end
 
 # AppDev Gems
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,70 +4,52 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "capybara/rails"
 require "capybara/rspec"
-# Prevent database truncation if the environment is production
+require 'database_cleaner/active_record'
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
-require "support/headless_chrome"
-# Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# Load support files
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |file| require file }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+# Ensure migrations are up to date
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
+# Register a custom Safari driver for Capybara
+Capybara.register_driver :selenium_safari do |app|
+  Capybara::Selenium::Driver.new(app, browser: :safari)
+end
+
+# Configure Capybara to use Safari for JavaScript tests
+Capybara.javascript_driver = :selenium_safari
+
 RSpec.configure do |config|
-  # Suppress RSpec warnings about potential false positives
-  RSpec::Expectations.configuration.on_potential_false_positives = :nothing
-  
-  # Shoulda matchers for association accessor specs
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  # Save test results and stop on first failure (optional)
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.fail_fast = false
+  config.formatter = :documentation
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # Capybara DSL globally accessible
+  config.include Capybara::DSL
+
+  # Fixture settings
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+  # Infer test type based on file location
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
+  # Clean database between tests
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
+
+  # Filter Rails backtrace
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
