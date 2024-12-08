29ca18d5451e37c8e58c2e7779c184bdf8d3e5c1
tluu5 <tluu5@uic.edu>
2024-12-08 17:48:25 -0600

    adding admin attrbutes CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
     namespace :admin do
       resources :locations, only: [:index, :new, :create, :edit, :update, :destroy]
       resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+      resources :attributes
     end
   end
 
